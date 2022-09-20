eea990f8e81142af4fd75c0cdff4803c0d8157ef
奇点 <3099234061@qq.com>
2022-09-20 12:41:30 +0800

    9.20

---
+++ src/work/WorkThree/WorkThree_3.java
@@ -0,0 +1,20 @@
+package work.WorkThree;
+
+public class WorkThree_3 {
+    public static void main(String[] args) {
+
+        int j = 1;
+        double sum = 0.0;
+        do {
+            double a = 1;
+            for (int i = 1; i <= j; i++) {
+                a *= i;
+            }
+            sum += (1.0 / a);
+            j++;
+        } while (j <= 20);
+        System.out.println(sum);
+
+
+    }
+}
